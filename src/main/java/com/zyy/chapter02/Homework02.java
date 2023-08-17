a4fe83670a453b103ead7c644b069fa5f9cacfb1
18314461596 <1394528597@qq.com>
2023-08-17 12:27:45 +0800

    1day

---
+++ src/main/java/com/zyy/chapter02/Homework02.java
@@ -0,0 +1,9 @@
+package com.zyy.chapter02;
+
+public class Homework02 {
+    //编写一个main方法
+    public static void main(String[] args){
+        //考察队转义字符
+        System.out.println("姓名\t性别\t籍贯\t住址\n顺平\t男\t四川\t北京");
+    }
+}
---
+++ src/main/java/com/zyy/chapter02/Temp2.java
@@ -0,0 +1,7 @@
+package com.zyy.chapter02;
+
+public class Temp2 {
+    public static void main(String[] args){
+        System.out.println("±±¾©");
+    }
+}
